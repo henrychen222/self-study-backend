7822575caf7b1448394df2cf49b8de4fd64e7408
Henry Chen <HenryChen@dswinc.com>
2020-02-13 13:19:38 -0500

    Demo Start

---
+++ ASPNETWeb/ASPNETWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNETWeb.MvcApplication" Language="C#" %>
--- Nodejs + Express/newIndividual3-node/sql/product.sql
+++
@@ -1,9 +0,0 @@
-use wchen;
-CREATE TABLE IF NOT EXISTS `PRODUCTS` (
-  `prd_id` int(11) NOT NULL,
-  `prd_name` varchar(20) NOT NULL,
-  `prd_price` varchar(10) NOT NULL
-);
-
-select * from PRODUCTS;
-desc PRODUCTS;
